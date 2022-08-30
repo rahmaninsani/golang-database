8981e830c6c7361a9a8ab699555a3ea68c0e35cb
Rahman Insani <rahmaninsani54@gmail.com>
2022-08-30 16:51:18 +0700

    Learn: QueryContext for SELECT

--- sql_test.go
+++ sql_test.go
@@ -28,3 +28,40 @@ func TestExecSql(t *testing.T) {
 	fmt.Println("Successfully executed query")
 
 }
+
+func TestQuerySql(t *testing.T) {
+	db := GetConnection()
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(db)
+
+	ctx := context.Background()
+
+	query := "SELECT id, name FROM customer"
+	rows, err := db.QueryContext(ctx, query)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer func(rows *sql.Rows) {
+		err := rows.Close()
+		if err != nil {
+
+		}
+	}(rows)
+
+	for rows.Next() {
+		var id, name string
+
+		err := rows.Scan(&id, &name)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("ID: %s, Name: %s\n", id, name)
+	}
+}
