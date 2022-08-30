74a102d046b93f6c30b638ce1929c45dd2173f56
Rahman Insani <rahmaninsani54@gmail.com>
2022-08-30 15:56:19 +0700

    Learn: Open Connection to MySQL

---
+++ database_test.go
@@ -0,0 +1,27 @@
+package golang_database
+
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"testing"
+)
+
+func TestEmpty(t *testing.T) {
+
+}
+
+func TestOpenConnection(t *testing.T) {
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/golang_database")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+
+		}
+	}(db)
+
+}
